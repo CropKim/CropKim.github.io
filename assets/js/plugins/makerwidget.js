f695aebcd7cf600718b95756fa924d6faf2051e4
cropkim <kjt0518@gmail.com>
2021-05-12 18:46:10 +0900

    updates

---
+++ assets/js/plugins/makerwidget.js
@@ -0,0 +1,12 @@
+(function(d, h, m){
+    var js, fjs = d.getElementsByTagName(h)[0];
+    if (d.getElementById(m)){return;}
+    js = d.createElement(h); js.id = m;
+    js.onload = function(){
+        window.makerWidgetComInit({
+        position: "right",          
+        widget: "00yz27k5jpoms7qy-rwvdjyhj7cmybaw7-tk6ktxsjw4vyjgol"                 
+    })};
+    js.src = "https://makerwidget.com/js/embed.js";
+    fjs.parentNode.insertBefore(js, fjs)
+}(document, "script", "dhm"));
